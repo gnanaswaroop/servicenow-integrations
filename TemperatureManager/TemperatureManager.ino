78b22a2b167dc7164a570ba32543bf0652d0e99f
swaroop.tg <swaroop.tg@servicenow.com>
2017-09-26 11:31:38 +0530

    Made more changes to post data to service now and to control the FAN

--- TemperatureManager/TemperatureManager.ino
+++ TemperatureManager/TemperatureManager.ino
@@ -4,20 +4,20 @@
 #include "LedMatrix.h"
 #include "DHT.h"
 
-const char* thingerUser = "XXXX";
-const char* deviceCode = "XXXX";
-const char* deviceName = "XXXX";
+const char* thingerUser = "xxxxx"; // FIXME: Replace with Real authentication
+const char* deviceCode = "xxxxx"; // FIXME: Replace with Real authentication
+const char* deviceName = "xxxxx"; // FIXME: Replace with Real authentication
 
 #define ONBOARDLED D0
 
 // LED Screen
-#define NUMBER_OF_DEVICES 1
+#define NUMBER_OF_DEVICES 2
 #define CS_PIN D4
 LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CS_PIN);
 
 // WiFi access point details.
-char *ssid = "XXXX";
-char *password = "XXXX";
+char *ssid = "xxxxx"; // FIXME: Replace with Real authentication
+char *password = "xxxxx"; // FIXME: Replace with Real authentication
 
 // DH11 pin connections
 #define DHTPin D3     // what digital pin we're connected to
@@ -31,36 +31,54 @@ DHT dht(DHTPin, DHTTYPE);
 // Thinger related
 ThingerESP8266 *thing;
 
+// Supports doing action every 'x' seconds
 unsigned long timer;
+
+const char* host = "XXXX.service-now.com"; // FIXME: Replace with Real ServiceNow instance
+const int httpsPort = 443;
+
+String lastReadTemperature = String("");
+
+#define FAN D1
+
 
 void setup()
 {
   Serial.begin(115200);
   timer = millis();
+  pinMode(FAN, OUTPUT);
+
 
   connectToWiFi();
   initializeThingerAPI();
-  Serial.println("Connected to Thinger");
-
-  dht.begin();
-  Serial.println("DH11 initialized");
-
+  initializeTemperatureSensor();
   initializeLEDScreen();
 }
 
 void initializeLEDScreen() {
   ledMatrix.init();
   ledMatrix.setIntensity(4); // range is 0-15
-  ledMatrix.setText("MAX7219 Demo"); // TODO: Will be cleaned up
+  ledMatrix.setText("Temperature Manager");
+  Serial.println("LED Screen Initialized");
+}
+
+void initializeTemperatureSensor() {
+
+  dht.begin();
+  Serial.println("DH11 initialized");
 }
 
 // Connect to Thinger.io and initialize end points.
 void initializeThingerAPI() {
   thing =  new ThingerESP8266(thingerUser, deviceName, deviceCode);
 
+  // TODO: Remove these dummy methods and add as per the requirement. 
   (*thing)["led_glow"] << [](pson & in) {
     Serial.println("led_glow triggered");
   };
+
+  Serial.println("Connected to Thinger");
+
 }
 
 
@@ -70,49 +88,60 @@ void connectToWiFi() {
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    //blink(ONBOARDLED, 500, 500);
+    blink(ONBOARDLED, 500, 500);
     delay(500);
   }
-  Serial.println("");
+  Serial.println("Connected to WiFi");
   Serial.println("IP Address : ");
   Serial.println(WiFi.localIP());
 }
 
-void readTemperatureSensor() {
-
-  float h = dht.readHumidity();
-  Serial.println("Humidity ");
-  Serial.println(h);
-
+// Read the temperature sensor and return the value as a String
+String readTemperatureSensor() {
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  Serial.println("Celsius ");
-  Serial.println(t);
 
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-  Serial.println("Fahrenheit ");
-  Serial.println(f);
+  int t = dht.readTemperature();
+
+  if (isnan(t) || t > 50) {
+    Serial.println("Invalid Temperature or NAN detected, will not utilize this read, returns the last reading. ");
+    return lastReadTemperature;
+  }
+
+  Serial.println("Celsius ");
+  lastReadTemperature = String(t);
+  Serial.println(lastReadTemperature);
+  ledMatrix.setText(lastReadTemperature);
+
+  return lastReadTemperature;
 }
 
-void doActionEvery(int afterMilliSeconds) {
-  if(millis() - timer > afterMilliSeconds) {
+// Returns true if an action was taken.
+bool doActionEvery(int afterMilliSeconds) {
+  // Only execute if the time has elapsed the parameter milliseconds
+  if (millis() - timer > afterMilliSeconds) {
     Serial.print("Timing : ");
     Serial.println(timer);
     timer = millis();
 
     // Keep flashing to show alive state
-    readTemperatureSensor();
-
+    String temperatureReading = readTemperatureSensor();
+    blink(ONBOARDLED, 500, 500);
+    uploadTemperature("DEVICE", temperatureReading);
+    digitalWrite(FAN, !digitalRead(FAN));
+    return true;
   }
-
+  return false;
 }
 
 void loop()  {
-  (*thing).handle();
+
+  // If the action time is not elapsed, then execute handle(), else execute Thinger.
+  if (!doActionEvery(10 * 1000)) {
+    // (*thing).handle(); // Temporarily disabling this as its failing when making REST POST calls.
+  }
+
   writeTextToDisplay();
-  doActionEvery(5000);
-  blink(ONBOARDLED, 500, 500);
+
 }
 
 void writeTextToDisplay() {
@@ -121,6 +150,56 @@ void writeTextToDisplay() {
   ledMatrix.drawText();
   ledMatrix.commit();
   delay(200);
+}
+
+// register the device on servicenow instance with its MAC address.
+void uploadTemperature(String device_id, String temperatureAsString) {
+
+  Serial.println("Starting to upload temperature to a ServiceNow instance ");
+
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("Failed to connect to a ServiceNow instance");
+    return;
+  }
+
+  Serial.println("Connected to a ServiceNow instance ");
+
+
+  String url = "/api/now/table/XXXXXX"; // FIXME: Replace with the Table to write to.
+  String jsonContent = "{'device_identifier': '" + device_id + "','temperature':'" + temperatureAsString + "'}";
+
+  Serial.println("Sending Temperature to ServiceNow Instance ");
+  Serial.println(jsonContent);
+
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: ESP8266\r\n" +
+               "Content-Type: application/json\r\n" +
+               "Accept: application/json\r\n" +
+               "Authorization: Basic XXXXXXXXr\n" + // FIXME: Replace with Real authentication (username:password Base64)
+               "Content-Length: " + jsonContent.length() + "\r\n" +
+               "Connection: close\r\n\r\n" +
+               jsonContent);
+
+  // bypass HTTP headers
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    //Serial.println( "Header: " + line );
+    if (line == "\r") {
+      break;
+    }
+  }
+  //  read body length
+  int bodyLength = 0;
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    //Serial.println( "Body length: " + line );
+    bodyLength = line.toInt();
+    break;
+  }
+
 }
 
 // Blinks an LED and again turns it off after 'x' seconds.
