e6ca52035badf2818de527d104d0a6793b16ace0
swaroop.tg <swaroop.tg@servicenow.com>
2017-09-27 18:00:39 +0530

    More changes to turn ON and OFF fan automatically on reaching threshold and also upload FAN ON and OFF state to the server.

--- TemperatureManager/TemperatureManager.ino
+++ TemperatureManager/TemperatureManager.ino
@@ -4,9 +4,9 @@
 #include "LedMatrix.h"
 #include "DHT.h"
 
-const char* thingerUser = "xxxxx"; // FIXME: Replace with Real authentication
-const char* deviceCode = "xxxxx"; // FIXME: Replace with Real authentication
-const char* deviceName = "xxxxx"; // FIXME: Replace with Real authentication
+const char* thingerUser = "XXXXX";
+const char* deviceCode = "XXXXX";
+const char* deviceName = "XXXXX";
 
 #define ONBOARDLED D0
 
@@ -16,31 +16,35 @@ const char* deviceName = "xxxxx"; // FIXME: Replace with Real authentication
 LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CS_PIN);
 
 // WiFi access point details.
-char *ssid = "xxxxx"; // FIXME: Replace with Real authentication
-char *password = "xxxxx"; // FIXME: Replace with Real authentication
+char *ssid = "XXXXX";
+char *password = "XXXXX";
 
 // DH11 pin connections
 #define DHTPin D3     // what digital pin we're connected to
 #define DHTTYPE DHT11   // DHT 11
-static char celsiusTemp[7];
-static char fahrenheitTemp[7];
-static char humidityTemp[7];
-
 DHT dht(DHTPin, DHTTYPE);
-
-// Thinger related
-ThingerESP8266 *thing;
 
 // Supports doing action every 'x' seconds
 unsigned long timer;
 
-const char* host = "XXXX.service-now.com"; // FIXME: Replace with Real ServiceNow instance
+const char* host = "XXXXX.service-now.com";
 const int httpsPort = 443;
+const String auth = "XXXXX"; // Base64
 
 String lastReadTemperature = String("");
 
 #define FAN D1
 
+int thresholdTemperature = 28; // In celsius
+
+const int NORMAL_INTERVAL = 10 * 1000;
+const int EMERGENCY_INTERVAL = 5 * 1000;
+
+int selectedInterval = NORMAL_INTERVAL;
+
+const int MAX_VALID_ROOM_TEMPERATURE = 50;
+
+const String DEVICE_ID = "DEVICE"; // This can be retrieved as a MAC Address. Hard-coded for simplicity
 
 void setup()
 {
@@ -48,11 +52,24 @@ void setup()
   timer = millis();
   pinMode(FAN, OUTPUT);
 
-
   connectToWiFi();
-  initializeThingerAPI();
+  turnFANOff();
+
   initializeTemperatureSensor();
   initializeLEDScreen();
+}
+
+void loop()  {
+  doActionEvery(selectedInterval);
+  writeTextToDisplay();
+}
+
+void writeTextToDisplay() {
+  ledMatrix.clear();
+  ledMatrix.scrollTextLeft();
+  ledMatrix.drawText();
+  ledMatrix.commit();
+  delay(200);
 }
 
 void initializeLEDScreen() {
@@ -63,24 +80,9 @@ void initializeLEDScreen() {
 }
 
 void initializeTemperatureSensor() {
-
   dht.begin();
   Serial.println("DH11 initialized");
 }
-
-// Connect to Thinger.io and initialize end points.
-void initializeThingerAPI() {
-  thing =  new ThingerESP8266(thingerUser, deviceName, deviceCode);
-
-  // TODO: Remove these dummy methods and add as per the requirement. 
-  (*thing)["led_glow"] << [](pson & in) {
-    Serial.println("led_glow triggered");
-  };
-
-  Serial.println("Connected to Thinger");
-
-}
-
 
 void connectToWiFi() {
   WiFi.begin(ssid, password);
@@ -96,27 +98,64 @@ void connectToWiFi() {
   Serial.println(WiFi.localIP());
 }
 
+// Write the Low bit to turn on the fan (via relay)
+void turnFanON() {
+  Serial.println("FAN turning ON");
+  digitalWrite(FAN, LOW);
+  uploadFanState(DEVICE_ID, true);
+  // selectedInterval = EMERGENCY_INTERVAL;
+}
+
+// Write the High bit to turn on the fan (via relay)
+void turnFANOff() {
+  Serial.println("FAN turning OFF");
+  digitalWrite(FAN, HIGH);
+  uploadFanState(DEVICE_ID, false);
+  // selectedInterval = NORMAL_INTERVAL;
+}
+
+boolean isFANOn() {
+  bool fanStatus = digitalRead(FAN);
+  Serial.println();
+
+  Serial.print("Is Fan on right now - ");
+  Serial.println(!fanStatus);
+
+  return !fanStatus;
+}
+
 // Read the temperature sensor and return the value as a String
 String readTemperatureSensor() {
   // Read temperature as Celsius (the default)
+  int currentTemperature = dht.readTemperature();
 
-  int t = dht.readTemperature();
-
-  if (isnan(t) || t > 50) {
-    Serial.println("Invalid Temperature or NAN detected, will not utilize this read, returns the last reading. ");
+  // Return last read temperature if invalid temperature is detected.
+  if (isnan(currentTemperature) || currentTemperature > MAX_VALID_ROOM_TEMPERATURE) {
+    Serial.print("Invalid Temperature or NAN detected, will not utilize this read, returns the last reading. ");
+    Serial.println(currentTemperature);
     return lastReadTemperature;
   }
 
-  Serial.println("Celsius ");
-  lastReadTemperature = String(t);
-  Serial.println(lastReadTemperature);
-  ledMatrix.setText(lastReadTemperature);
+  lastReadTemperature = String(currentTemperature);
+
+  Serial.print("Temperature detected in celsius ");
+  Serial.println(currentTemperature);
+
+  // If the threshold is crossed, keep the fan on.
+  if (currentTemperature >= thresholdTemperature && !isFANOn()) {
+    Serial.println("Temperature exceeds threshold temperature ");
+    turnFanON();
+  } else if (currentTemperature < thresholdTemperature && isFANOn()) { // If the temperature falls, turn off the FAN if its already ON.
+    Serial.println("Temperature returns back to less than threshold temperature ");
+    turnFANOff();
+  }
 
   return lastReadTemperature;
 }
 
 // Returns true if an action was taken.
 bool doActionEvery(int afterMilliSeconds) {
+
   // Only execute if the time has elapsed the parameter milliseconds
   if (millis() - timer > afterMilliSeconds) {
     Serial.print("Timing : ");
@@ -125,32 +164,17 @@ bool doActionEvery(int afterMilliSeconds) {
 
     // Keep flashing to show alive state
     String temperatureReading = readTemperatureSensor();
+
+    // Set to Display.
+    ledMatrix.setText(temperatureReading);
+
     blink(ONBOARDLED, 500, 500);
     uploadTemperature("DEVICE", temperatureReading);
-    digitalWrite(FAN, !digitalRead(FAN));
     return true;
   }
-  return false;
+  // return false;
 }
 
-void loop()  {
-
-  // If the action time is not elapsed, then execute handle(), else execute Thinger.
-  if (!doActionEvery(10 * 1000)) {
-    // (*thing).handle(); // Temporarily disabling this as its failing when making REST POST calls.
-  }
-
-  writeTextToDisplay();
-
-}
-
-void writeTextToDisplay() {
-  ledMatrix.clear();
-  ledMatrix.scrollTextLeft();
-  ledMatrix.drawText();
-  ledMatrix.commit();
-  delay(200);
-}
 
 // register the device on servicenow instance with its MAC address.
 void uploadTemperature(String device_id, String temperatureAsString) {
@@ -161,13 +185,14 @@ void uploadTemperature(String device_id, String temperatureAsString) {
   WiFiClientSecure client;
   if (!client.connect(host, httpsPort)) {
     Serial.println("Failed to connect to a ServiceNow instance");
+
     return;
   }
 
   Serial.println("Connected to a ServiceNow instance ");
 
 
-  String url = "/api/now/table/XXXXXX"; // FIXME: Replace with the Table to write to.
+  String url = "/api/now/table/XXXXX"; // Change the table name
   String jsonContent = "{'device_identifier': '" + device_id + "','temperature':'" + temperatureAsString + "'}";
 
   Serial.println("Sending Temperature to ServiceNow Instance ");
@@ -178,7 +203,7 @@ void uploadTemperature(String device_id, String temperatureAsString) {
                "User-Agent: ESP8266\r\n" +
                "Content-Type: application/json\r\n" +
                "Accept: application/json\r\n" +
-               "Authorization: Basic XXXXXXXXr\n" + // FIXME: Replace with Real authentication (username:password Base64)
+               "Authorization: Basic " + auth + "\r\n" +
                "Content-Length: " + jsonContent.length() + "\r\n" +
                "Connection: close\r\n\r\n" +
                jsonContent);
@@ -200,6 +225,61 @@ void uploadTemperature(String device_id, String temperatureAsString) {
     break;
   }
 
+  Serial.println("Completed sending Temperature to ServiceNow Instance ");
+
+
+}
+
+// register the device on servicenow instance with its MAC address.
+void uploadFanState(String device_id, boolean fanState) {
+
+  Serial.println("Starting to upload fan state to a ServiceNow instance ");
+
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("Failed to connect to a ServiceNow instance");
+
+    return;
+  }
+
+  Serial.println("Connected to a ServiceNow instance ");
+
+
+  String url = "/api/now/table/XXXXX"; // Change the table name
+  String jsonContent = "{'u_device_identifier': '" + device_id + "','u_fan_state':'" + (fanState == true ? "ON" : "OFF") + "'}";
+
+  Serial.println("Sending Fan State to ServiceNow Instance ");
+  Serial.println(jsonContent);
+
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: ESP8266\r\n" +
+               "Content-Type: application/json\r\n" +
+               "Accept: application/json\r\n" +
+               "Authorization: Basic " + auth + "\r\n" +
+               "Content-Length: " + jsonContent.length() + "\r\n" +
+               "Connection: close\r\n\r\n" +
+               jsonContent);
+
+  // bypass HTTP headers
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    //Serial.println( "Header: " + line );
+    if (line == "\r") {
+      break;
+    }
+  }
+  //  read body length
+  int bodyLength = 0;
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    //Serial.println("Body length: " + line );
+    bodyLength = line.toInt();
+    break;
+  }
+
+  Serial.println("Completed sending Fan State to ServiceNow Instance ");
 }
 
 // Blinks an LED and again turns it off after 'x' seconds.
